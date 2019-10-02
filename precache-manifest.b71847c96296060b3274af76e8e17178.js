016f35a1df1ebdc7895a2a7b3532b2a964182af9
Aldu <exaldo@hotmail.com>
2019-10-01 22:29:08 -0500

    Updates

---
+++ precache-manifest.b71847c96296060b3274af76e8e17178.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbfdbbb8a60f0394f3cbcf36455a4b0",
+    "url": "/tiendangalum/index.html"
+  },
+  {
+    "revision": "d09647d18c72f5d91996",
+    "url": "/tiendangalum/static/css/main.04aad8a2.chunk.css"
+  },
+  {
+    "revision": "ac693b8f51229cd712e1",
+    "url": "/tiendangalum/static/js/2.293e9d91.chunk.js"
+  },
+  {
+    "revision": "d09647d18c72f5d91996",
+    "url": "/tiendangalum/static/js/main.4f2374ca.chunk.js"
+  },
+  {
+    "revision": "b90f6330eac734db7e02",
+    "url": "/tiendangalum/static/js/runtime~main.dc4836f0.js"
+  },
+  {
+    "revision": "30ef34f598c22aaca89fdb2651fc9dc8",
+    "url": "/tiendangalum/static/media/customer-support.30ef34f5.svg"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/tiendangalum/static/media/email.e6956db2.svg"
+  },
+  {
+    "revision": "902de655683726b0767053790845e9fd",
+    "url": "/tiendangalum/static/media/homebg.902de655.jpg"
+  },
+  {
+    "revision": "af47a02330f466d88ae7282d7dfa8405",
+    "url": "/tiendangalum/static/media/phone-book.af47a023.svg"
+  },
+  {
+    "revision": "4b0ff2f316d7dd9a40e222619ddddcd9",
+    "url": "/tiendangalum/static/media/placeholder-white.4b0ff2f3.svg"
+  },
+  {
+    "revision": "9693838299169e7d66c6702c5de7119c",
+    "url": "/tiendangalum/static/media/placeholder.96938382.svg"
+  },
+  {
+    "revision": "7f862e6e9f669a0159102cc0d783db7b",
+    "url": "/tiendangalum/static/media/racoon.7f862e6e.png"
+  }
+]);
