383f9950228400e3049a5f8863651075631d2907
Aldo Guillermo Rodríguez Madrigal <aldu@MacBook-Pro-de-Aldo.local>
2019-10-14 22:44:13 -0500

    Updates

---
+++ precache-manifest.1cc93897eb411cca483c82be92e4351d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7291ca2813d613416508169608bf5504",
+    "url": "/tiendangalum/index.html"
+  },
+  {
+    "revision": "45b5950087ebf84925c4",
+    "url": "/tiendangalum/static/css/main.c6cf3d1c.chunk.css"
+  },
+  {
+    "revision": "93ac88b66e1f417faed9",
+    "url": "/tiendangalum/static/js/2.ac4f5dfa.chunk.js"
+  },
+  {
+    "revision": "45b5950087ebf84925c4",
+    "url": "/tiendangalum/static/js/main.46b5beb5.chunk.js"
+  },
+  {
+    "revision": "b90f6330eac734db7e02",
+    "url": "/tiendangalum/static/js/runtime~main.dc4836f0.js"
+  },
+  {
+    "revision": "30ef34f598c22aaca89fdb2651fc9dc8",
+    "url": "/tiendangalum/static/media/customer-support.30ef34f5.svg"
+  },
+  {
+    "revision": "77c83e33aba37b11b107d86ae59c871a",
+    "url": "/tiendangalum/static/media/email.77c83e33.svg"
+  },
+  {
+    "revision": "902de655683726b0767053790845e9fd",
+    "url": "/tiendangalum/static/media/homebg.902de655.jpg"
+  },
+  {
+    "revision": "6cca863d555f51a60b97596d90f06d69",
+    "url": "/tiendangalum/static/media/phone-book.6cca863d.svg"
+  },
+  {
+    "revision": "05e3ab3378500bdb867b7785a62faff4",
+    "url": "/tiendangalum/static/media/placeholder-white.05e3ab33.svg"
+  },
+  {
+    "revision": "76fa68c45b9ae01541a21734c1f8d87c",
+    "url": "/tiendangalum/static/media/placeholder.76fa68c4.svg"
+  },
+  {
+    "revision": "7f862e6e9f669a0159102cc0d783db7b",
+    "url": "/tiendangalum/static/media/racoon.7f862e6e.png"
+  }
+]);
