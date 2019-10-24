ed82407626b8248da22bcfc8b15db509bd5ea4de
Aldu <exaldo@hotmail.com>
2019-10-24 14:47:35 -0500

    landing

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CardLinks from "../cardlinks/CardLinks";
-import { Container, Carousel, Row, Col } from "react-bootstrap";
+import { Container, Carousel, Row, Col, Image } from "react-bootstrap";
 import "./Home.css";
 // import img from '../../assets/Photos/carosel1.jpg'
 
@@ -8,6 +8,24 @@ const Home = () => {
   return (
     <React.Fragment>
       <Container className="home-c">
+        <Row>
+          <Col xs={12} sm={12} md={6} lg={6}>
+            <Image
+              src={require("../../assets/Photos/logo.png")}
+              alt="logo"
+              fluid
+            />
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={6}>
+            <h2 className="home-title">NG ALUMINIO Y VIDRIO</h2>
+            <p className="home-text1">
+              Seriedad y compromiso <br /> con nuestros clientes nos distinguen{" "}
+            </p>
+            <p className="home-text2">13 AÑOS DE EXPERIENCIA</p>
+            <p className="home-text3">OFERTAS Y PROMOCIONES</p>
+          </Col>
+        </Row>
+
         <Row>
           <Col xs={12} sm={12} md={12} lg={12}>
             <div id="in-home"></div>
